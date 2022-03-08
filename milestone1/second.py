7c438cd5dc682827143f92961ed0f4f586e03fe6
Navin <navin0707akd@gmail.com>
2022-03-08 17:23:08 +0530

    third

--- milestone1/second.py
+++ milestone1/second.py
@@ -1,58 +1,58 @@
 import yaml
 from yaml.loader import SafeLoader
-from datetime import datetime
+from datetime import datetime 
 import time
 import threading
  
-a_yaml_file = open("Milestone1A.yaml")
+a_yaml_file = open("Milestone1B.yaml")
 workflow= yaml.load(a_yaml_file, Loader=yaml.SafeLoader)
 
-file = open("log1.txt","w")
+file = open("log2.txt","w")
 threads= []
-def flow(obj, execution, activities):
-    if execution == "sequential":
+def flow(ob, execution, activities):
+    if execution == "Sequential":
         for n in activities:
             now = datetime.now()
-            file.write(f"{now};{obj}.{n} Entry\n")
+            file.write(f"{now};{ob}.{n} Entry\n")
             if activities[n]['Type'] == "Flow":
-                newwork = obj + '.' + n
+                newwork = ob + '.' + n
                 flow(newwork,activities[n]['Execution'],activities[n]['Activities'])
             elif activities[n]['Type'] == "Task":
-                newwork = obj + '.' + n
-                task(newwork,activities[n]['function'],activities[n]['Inputs'])
+                newwork = ob + '.' + n
+                task(newwork,activities[n]['Function'],activities[n]['Inputs'])
             now= datetime.now()
-            file.write(f"{now};{obj}.{n} Exit\n")
+            file.write(f"{now};{ob}.{n} Exit\n")
     if execution == "Concurrent":
         for n in activities:
             now = datetime.now()
-            file.write(f"{now};{obj}.{n} Entry\n")
+            file.write(f"{now};{ob}.{n} Entry\n")
             if activities[n]['Type'] == "Flow":
-                newwork = obj + '.' + n 
+                newobj = ob + '.' + n 
                 thread = threading.Thread(target=flow,args=[newwork,activities[n]['Execution'],activities[n]['Activities']])
                 thread.start()
                 thread.append({thread,newwork})
             elif  activities[n]['Type'] == "Task":
-                newwork = obj + '.' + n
+                newwork = ob + '.' + n
                 thread = threading.Thread(target=task,args=[newwork,activities[n]['Function'],activities[n]['Inputs']])  
                 thread.start()
                 thread.append({thread,newwork}) 
             now = datetime.now()
             for thread in threads:
                 thread.join()
-            file.write(f"{now};{obj}.{n} Exit\n")
-    def task(obj,function,inputs):
+            file.write(f"{now};{ob}.{n} Exit\n")
+def task(ob,function,inputs):
         if function == 'TimeFunction':
-          fun_input = inputs['FunctionInput']  
-          exc_time =inputs['ExecutionTime']
+            fun_input = inputs['FunctionInput']  
+            exc_time =inputs['ExecutionTime']
         now = datetime.now()
-        file.write(f"{now};{obj} Executing {function} ({fun_input}), {exc_time})\n")
+        file.write(f"{now};{ob} Executing {function} ({fun_input}), {exc_time})\n")
         time.sleep(int(exc_time))
-for obj in workflow:
+for ob in workflow:
         now = datetime.now()
-        file.write(F"{now};{obj} Entry\n")
-        if workflow[obj]['Type'] == "flow":
-            flow(obj,workflow[obj]['Execution'],workflow[obj]['Activities'])
-        elif workflow[obj]['Type'] == "Task":
-            task(obj,workflow[obj]['function'],workflow[obj]['Inputs'])   
+        file.write(F"{now};{ob} Entry\n")
+        if workflow[ob]['Type'] == "Flow":
+            flow(ob,workflow[ob]['Execution'],workflow[ob]['Activities'])
+        elif workflow[ob]['Type'] == "Task":
+            task(ob,workflow[ob]['function'],workflow[ob]['Inputs'])   
         now = datetime.now()
-        file.write(f"{now};{obj} Exit")    
+        file.write(f"{now};{ob} Exit")    
